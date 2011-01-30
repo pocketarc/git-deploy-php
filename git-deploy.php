70ad76e45765a3ef6f298cbdaca88cbb2dd2fdff
BrunoDeBarros <bruno@terraduo.com>
2011-01-30 18:15:41 +0000

    * Parse .ini files passed in arguments.

--- git-deploy.php
+++ git-deploy.php
@@ -1,3 +1,18 @@
 <?php
 
- 
+if (isset($argv[1])) {
+  $deploy = $argv[1];
+} else {
+  $deploy = 'deploy.ini';
+}
+
+if (!@file_exists($deploy)) {
+    trigger_error("File '$deploy' does not exist.");
+} else {
+    $servers = parse_ini_file($deploy); 
+    if (!$servers) {
+        trigger_error("File '$deploy' is not a valid .ini file.");
+    }
+}
+
+$revision = exec('git rev-parse HEAD'); 
