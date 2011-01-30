8e620cdcd8e4f1c6a902035cd6a4f098d00dbe2a
BrunoDeBarros <bruno@terraduo.com>
2011-01-30 20:33:24 +0000

    Finished everything.

--- git-deploy.php
+++ git-deploy.php
@@ -15,8 +15,6 @@ if (!@file_exists($deploy)) {
     }
 }
 
-$revision = exec('git rev-parse HEAD');
-
 foreach ($servers as $uri => $options) {
 
     if (substr($uri, 0, 6) === 'ftp://') {
@@ -61,6 +59,7 @@ function deployGitOverFtp($host, $user = '', $pass = '', $port = 21, $path = '/'
             throw new Exception("Could not login to $host (Tried to login as $user).");
         }
 
+
         ftp_pasv($connection, $passive);
 
         if (ftp_chdir($connection, $path)) {
@@ -68,6 +67,10 @@ function deployGitOverFtp($host, $user = '', $pass = '', $port = 21, $path = '/'
         } else {
             throw new Exception("Could not change the FTP directory to $path.");
         }
+
+        echo "----------------------------\r\n";
+        echo "Connected to {$host}{$path}\r\n";
+        echo "----------------------------\r\n";
 
         # Now that we're logged in to the server, let's get the remote revision.
 
@@ -94,7 +97,51 @@ function deployGitOverFtp($host, $user = '', $pass = '', $port = 21, $path = '/'
         $output = array();
         exec($command, $output);
 
-        var_dump($output);
+        $filesToUpload = array();
+        $filesToDelete = array();
+
+        if (!empty($remoteRevision)) {
+            foreach ($output as $line) {
+                if ($line[0] == 'A' or $line[0] == 'C' or $line[0] == 'M') {
+                    $filesToUpload[] = trim(substr($line, 1, strlen($line)));
+                } elseif ($line[0] == 'D') {
+                    $filesToDelete[] = trim(substr($line, 1, strlen($line)));
+                } else {
+                    throw new Exception("Unknown git-diff status: {$line[0]}");
+                }
+            }
+        } else {
+            $filesToUpload = $output;
+        }
+
+        foreach ($filesToUpload as $file) {
+            # Make sure the folder exists in the FTP server.
+
+            $origin = ftp_pwd($connection);
+            $dir = dirname($file);
+
+            if (!@ftp_chdir($connection, $dir)) {
+                ftp_mkdir($connection, $dir);
+            }
+
+            ftp_chdir($connection, $origin);
+
+            ftp_put($connection, $file, $file, FTP_BINARY);
+            echo "Uploaded {$file}\r\n";
+        }
+
+        foreach ($filesToDelete as $file) {
+            ftp_delete($connection, $file);
+            echo "Deleted {$file}\r\n";
+        }
+
+        $temp = tempnam(sys_get_temp_dir(), 'gitRevision');
+        file_put_contents($temp, exec('git rev-parse HEAD'));
+        ftp_put($connection, 'REVISION', $temp, FTP_BINARY);
+        unlink($temp);
+        echo "Uploaded REVISION file\r\n";
+
+        echo "Finished working on {$host}{$path}\r\n";
 
         ftp_close($connection);
     }
