a0e05a8c9baefd46b2f8f605d8ebea8cffcf7fc4
Bruno Moreira De Barros <bruno@terraduo.com>
2015-02-07 15:11:38 +0000

    Adds a check for phar.readonly and a note on fixing it.

--- tools/build.php
+++ tools/build.php
@@ -1,10 +1,14 @@
 #!/usr/bin/env php
 <?php
 
-ini_set("phar.readonly", "Off");
-$phar = new Phar('git-deploy.phar', 0, 'git-deploy');
-$phar->buildFromDirectory(dirname(__FILE__));
-$phar->setStub(file_get_contents(dirname(__FILE__)."/index.php"));
-unset($phar);
-@unlink('../git-deploy');
-rename('git-deploy.phar', '../git-deploy');
+if (ini_get("phar.readonly")) {
+    echo "You need to set the 'phar.readonly' option to 'Off' in your php.ini file (" . php_ini_loaded_file() . ")".PHP_EOL;
+} else {
+    $phar = new Phar('git-deploy.phar', 0, 'git-deploy');
+    $phar->buildFromDirectory(dirname(__FILE__));
+    $phar->setStub(file_get_contents(dirname(__FILE__) . "/index.php"));
+    unset($phar);
+    @unlink('../git-deploy');
+    rename('git-deploy.phar', '../git-deploy');
+    echo "Built git-deploy successfully!".PHP_EOL;
+}
